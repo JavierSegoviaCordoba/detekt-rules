b09dc2c1cb13e89d91b1cd5d1103a3ac38aee9b1
Javier Segovia Córdoba <javier@segoviacordoba.com>
2021-08-23 11:34:49 +0200

    Add Gradle config files

---
+++ a--catalogs/libs/build.gradle.kts
@@ -0,0 +1,14 @@
+@file:Suppress("PropertyName", "VariableNaming")
+
+// Catalog name: libs
+
+// [versions]
+val detekt = "1.18.0"
+val kotest = "4.6.1"
+
+// [libraries]
+val gitlab_arturboschDetekt_detektApi = "io.gitlab.arturbosch.detekt:detekt-api:$detekt"
+val gitlab_arturboschDetekt_detektTest = "io.gitlab.arturbosch.detekt:detekt-test:$detekt"
+val jetbrains_kotlin_kotlinTest = "org.jetbrains.kotlin:kotlin-test"
+val jetbrains_kotlin_kotlinTestJunit = "org.jetbrains.kotlin:kotlin-test-junit"
+val kotest_kotestAssertionsCore = "io.kotest:kotest-assertions-core:$kotest"
---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+dependencies {
+    implementation(gradleApi())
+    implementation(localGroovy())
+
+    // TODO: remove when the next issue is fixed:
+    //  https://github.com/gradle/gradle/issues/15383#issuecomment-779893192
+    implementation(files(libs.javaClass.superclass.protectionDomain.codeSource.location))
+    implementation(files(pluginLibs.javaClass.superclass.protectionDomain.codeSource.location))
+
+    pluginLibs.apply {
+        implementation(javiersc.gradlePlugins.allProjects)
+        implementation(javiersc.gradlePlugins.buildVersionCatalogsUpdater)
+        implementation(javiersc.gradlePlugins.changelog)
+        implementation(javiersc.gradlePlugins.codeAnalysis)
+        implementation(javiersc.gradlePlugins.codeFormatter)
+        implementation(javiersc.gradlePlugins.dependencyUpdates)
+        implementation(javiersc.gradlePlugins.docs)
+        implementation(javiersc.gradlePlugins.gradleWrapperUpdater)
+        implementation(javiersc.gradlePlugins.kotlinMultiplatform)
+        implementation(javiersc.gradlePlugins.nexus)
+        implementation(javiersc.gradlePlugins.publishKotlinJvm)
+        implementation(javiersc.gradlePlugins.readmeBadges)
+        implementation(javiersc.gradlePlugins.versioning)
+
+        implementation(jetbrains.kotlin.kotlinGradlePluginX)
+        implementation(jetbrains.kotlinx.binaryCompatibilityValidator)
+    }
+}
